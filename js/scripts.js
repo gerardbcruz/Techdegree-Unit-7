8b41dd1f0f991c07e4e3791be92cfb0cd25368b2
Gerard Cruz <gerardbcruz@gmail.com>
2020-01-29 22:58:50 -0500

    Completed Messaging Auto-complete Search

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,38 @@
 
-// HTML Nodes
+/////////// VARIABLES ///////////
+
+// Notification Bell
+const bell = document.querySelector('.bell');
+const notifications = document.querySelector('.notifications');
+
+// Alert Banner
 const alertBanner = document.getElementById('alert');
+
+// Traffic Charts
 const trafficCanvas = document.getElementById('traffic-chart');
 const dailyCanvas = document.getElementById('daily-traffic-chart');
 const mobileCanvas = document.getElementById('mobile-users-chart');
+
+// Traffic Chart Navigation
+const trafficNav = document.querySelector('.traffic-nav');
+const trafficNavLinks = document.querySelectorAll('.traffic-nav-link');
+
+// Messaging Section
 const user = document.getElementById("user-field");
 const message = document.getElementById("form-field");
 const send = document.getElementById("send");
 
-const bell = document.querySelector('.bell');
-const notifications = document.querySelector('.notifications');
-
-const trafficNav = document.querySelector('.traffic-nav');
-const trafficNavLinks = document.querySelectorAll('.traffic-nav-link');
-
-
-// Traffic Chart Nav Event Listener
-trafficNav.addEventListener("click", (e) => {
-  trafficNavLink = e.target;
-  //Remove all active classes on trafficNavLinks
-  for (let i=0; i < trafficNavLinks.length; i++) {
-    trafficNavLinks[i].classList.remove("active");
-  }
-  //
-  if (trafficNavLink.textContent === 'Hourly') {
-    createTrafficChart(trafficDataHourly);
-    trafficNavLink.classList.add("active");
-  } else if (trafficNavLink.textContent === 'Daily') {
-    createTrafficChart(trafficDataDaily);
-    trafficNavLink.classList.add("active");
-  } else if (trafficNavLink.textContent === 'Weekly') {
-    createTrafficChart(trafficDataWeekly);
-    trafficNavLink.classList.add("active");
-  } else if (trafficNavLink.textContent === 'Monthly') {
-    createTrafficChart(trafficDataMonthly);
-    trafficNavLink.classList.add("active");
-  }
-})
-
-
-// Notifications Drop Down
-notifications.style.display = "none";
-
-bell.addEventListener("click", () => {
-  if (notifications.style.display === "none") {
-    notifications.style.display = "block";
-  } else if (notifications.style.display === "block") {
-    notifications.style.display = "none";
-  }
-})
-
+// Messaging Autocomplete
+const autoComplete = document.querySelector('.autocomplete');
+const autoCompleteNames = [
+  'Dan Byrd',
+  'Dawn Wood',
+  'Dan Oliver',
+  'Jasmine Rodriguez',
+  'Melanie Castillo',
+  'Vicky Reinhart',
+  'Victoria Chambers'
+];
 
 // Traffic Line Chart Data and Options
 const trafficDataHourly = {
@@ -131,10 +114,6 @@ const trafficOptions = {
       }
 }
 
-// Initialize Page with Traffic Data - Weekly
-createTrafficChart(trafficDataWeekly);
-
-
 // Daily Bar Chart Data and Options
 const dailyData = {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -184,37 +163,7 @@ const mobileOptions = {
     }
 }
 
-
-// Create Alert Banner on page load
-alertBanner.innerHTML =
-`
-<div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
-<p class="alert-banner-close">X</p>
-</div>`
-
-// Event Listener on alert banner close button
-alertBanner.addEventListener("click", (e) => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-      alertBanner.style.display = "none";
-    }
-})
-
-// Event Listener on Messaging Widget Send button
-send.addEventListener("click", (e) => {
-  if (user.value === '' && message.value === '') {
-    alert("Please fill out both user and message fields");
-  } else if (user.value === '') {
-    alert("Please fill out the user field");
-  } else if (message.value === '') {
-    alert("Please fill out the message field");
-  } else {
-    alert(`Message successfully sent to ${user.value}`)
-  }
-})
-
+/////////// FUNCTIONS ///////////
 
 // Create Traffic Line Chart
 function createTrafficChart(trafficData) {
@@ -228,28 +177,134 @@ function createTrafficChart(trafficData) {
   });
 }
 
+function initializePage() {
+  // Create Alert Banner on page load
+  alertBanner.innerHTML =
+  `
+  <div class="alert-banner">
+  <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+  to complete</p>
+  <p class="alert-banner-close">X</p>
+  </div>`
 
-// Create Bar Graph Chart
-const dailyChart = new Chart(dailyCanvas, {
-    // The type of chart we want to create
-    type: 'bar',
-    // The data for our dataset
-    data: dailyData,
-    // Configuration options go here
-    options: dailyOptions
-});
+  // Hide Search Autocomplete
+  autoComplete.style.display = "none";
+
+  // Hide Notifications Drop Down
+  notifications.style.display = "none";
+
+  // Initialize Page with Traffic Data - Weekly
+  createTrafficChart(trafficDataWeekly);
+
+  // Create Bar Graph Chart
+  const dailyChart = new Chart(dailyCanvas, {
+      // The type of chart we want to create
+      type: 'bar',
+      // The data for our dataset
+      data: dailyData,
+      // Configuration options go here
+      options: dailyOptions
+  });
+
+  // Create Donut Graph Chart
+  const donutChart = new Chart(mobileCanvas, {
+      // The type of chart we want to create
+      type: 'doughnut',
+      // The data for our dataset
+      data: mobileData,
+      // Configuration options go here
+      options: mobileOptions
+  });
+}
+
+/////////// INITIALIZE PAGE ///////////
+initializePage();
 
 
-// Create Donut Graph Chart
-const donutChart = new Chart(mobileCanvas, {
-    // The type of chart we want to create
-    type: 'doughnut',
-    // The data for our dataset
-    data: mobileData,
-    // Configuration options go here
-    options: mobileOptions
-});
+/////////// EVENT LISTENERS ///////////
 
+// Notifications Event Listener
+bell.addEventListener("click", () => {
+  if (notifications.style.display === "none") {
+    notifications.style.display = "block";
+  } else if (notifications.style.display === "block") {
+    notifications.style.display = "none";
+  }
+})
 
+// Alert banner close button Event Listener
+alertBanner.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+      alertBanner.style.display = "none";
+    }
+})
 
-//TESTING
+// Traffic Chart Nav Event Listener
+trafficNav.addEventListener("click", (e) => {
+  trafficNavLink = e.target;
+  //Remove all active classes on trafficNavLinks
+  for (let i=0; i < trafficNavLinks.length; i++) {
+    trafficNavLinks[i].classList.remove("active");
+  }
+  //
+  if (trafficNavLink.textContent === 'Hourly') {
+    createTrafficChart(trafficDataHourly);
+    trafficNavLink.classList.add("active");
+  } else if (trafficNavLink.textContent === 'Daily') {
+    createTrafficChart(trafficDataDaily);
+    trafficNavLink.classList.add("active");
+  } else if (trafficNavLink.textContent === 'Weekly') {
+    createTrafficChart(trafficDataWeekly);
+    trafficNavLink.classList.add("active");
+  } else if (trafficNavLink.textContent === 'Monthly') {
+    createTrafficChart(trafficDataMonthly);
+    trafficNavLink.classList.add("active");
+  }
+})
+
+// Messaging Widget Send button Event Listener
+send.addEventListener("click", (e) => {
+  if (user.value === '' && message.value === '') {
+    alert("Please fill out both user and message fields");
+  } else if (user.value === '') {
+    alert("Please fill out the user field");
+  } else if (message.value === '') {
+    alert("Please fill out the message field");
+  } else {
+    alert(`Message successfully sent to ${user.value}`)
+  }
+})
+
+// Autocomplete Input Event Listener
+user.addEventListener("keyup", () => {
+  const searchValue = user.value.toLowerCase();
+  autoComplete.innerHTML = "";
+  autoComplete.style.display = "none";
+
+  // Loop through autoCompleteNames
+  for (let i=0; i < autoCompleteNames.length; i++) {
+
+    const lowerCaseName = autoCompleteNames[i].toLowerCase();
+    let li = document.createElement("li");
+    li.textContent = autoCompleteNames[i];
+    autoComplete.appendChild(li);
+
+    // Check if input is not empty & Search Value matches the Array names
+    if (searchValue && lowerCaseName.indexOf(searchValue) > -1) {
+      autoComplete.style.display = "block";
+      li.style.display = "block";
+      console.log(searchValue);
+      console.log(lowerCaseName);
+    } else {
+      li.style.display = "none";
+    }
+  }
+})
+
+// Adds autosuggestion to input box when clicked
+autoComplete.addEventListener("click", (e) => {
+  li = e.target;
+  user.value = li.textContent;
+  autoComplete.style.display = "none";
+})
